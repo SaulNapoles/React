25ca4a6918db68f8559e92b87fac43e935ce66b7
SaulNapoles <saul_antonio_16@hotmail.com>
2022-10-23 00:04:59 -0700

    Termino de Hooks personalizados y Hook UseRef

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -1,5 +1,5 @@
-import { useCounter } from '../hooks/useCounter';
-import { useFetch } from '../hooks/useFetch'
+import { useCounter, useFetch } from '../hooks';
+import { LoadingQuote, Quote } from './';
 
 const initialValue = 1;
 
@@ -10,7 +10,7 @@ export const MultipleCustomHooks = () => {
     const { counter, increment, decrement } = useCounter( initialValue )
     const { data, isLoading, hasError } = useFetch(`https://breakingbadapi.com/api/quotes/${ counter }`)
 
-    const { author, quote } = !!data && data[0];
+    const { quote_id, author, quote } = !!data && data[0];
     console.log(data);  
 
 
@@ -25,26 +25,26 @@ export const MultipleCustomHooks = () => {
             // Operador ternario 
             (isLoading) 
                 ? (
-                    <div className="alert alert-info text-center">
-                        Loading...
-                    </div>
+                    <LoadingQuote/>
                 )
                 : (
-                    <blockquote className="blockquote text-end">
-                        <p className="mb-1">{ author }</p>
-                        <footer className="blockquote-footer">{ quote }</footer>
-                    </blockquote>
+                    <Quote author={ author } quote={ quote }/>
                 )
         }
+        
+        
+
 
         <button 
             className="btn btn-primary" 
+            disabled={ isLoading }
             onClick={ () => increment() }>
             Next quote
         </button>
 
         <button 
             className="btn btn-primary" 
+            disabled={ quote_id === 1 || isLoading }
             onClick={ () => decrement() }>
             Previus quote
         </button>
