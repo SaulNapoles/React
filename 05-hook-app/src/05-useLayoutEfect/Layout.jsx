dc2942ed359ae9658632f17c93da65605e030eef
SaulNapoles <saul_antonio_16@hotmail.com>
2022-11-07 21:22:02 -0800

    memo en showIncremet

---
+++ 05-hook-app/src/05-useLayoutEfect/Layout.jsx
@@ -0,0 +1,51 @@
+import { useCounter, useFetch } from '../hooks';
+import { LoadingQuote, Quote } from '../03-examples';
+
+const initialValue = 1;
+
+export const Layout = () => {
+    
+    
+
+    const { counter, increment, decrement } = useCounter( initialValue )
+    const { data, isLoading, hasError } = useFetch(`https://breakingbadapi.com/api/quotes/${ counter }`)
+
+    const { quote_id, author, quote } = !!data && data[0];
+    console.log(data);  
+
+
+
+    return (
+    <>
+    
+        <h1>BreakingBad Quotes</h1>
+        <hr />
+
+        {
+            // Operador ternario 
+            isLoading
+                ?<LoadingQuote/>
+                :<Quote author={ author } quote={ quote }/>
+                
+        }
+        
+        
+
+
+        <button 
+            className="btn btn-primary" 
+            disabled={ isLoading }
+            onClick={ () => increment() }>
+            Next quote
+        </button>
+
+        <button 
+            className="btn btn-primary" 
+            disabled={ quote_id === 1 || isLoading }
+            onClick={ () => decrement() }>
+            Previus quote
+        </button>
+
+    </>
+  )
+}
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { CallbackHook } from './06-memos/CallbackHook'
+// import { MemoHook } from './06-memos/MemoHook'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { HooksApp } from './HooksApp'
-import { FocusScreen } from './04-useRef/FocusScreen'
+// import { FocusScreen } from './04-useRef/FocusScreen'
+// import { Layout } from './05-useLayoutEfect/Layout'
+// import { Memorize } from './06-memos/Memorize'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <FocusScreen/>
+    <CallbackHook/>
   // </React.StrictMode>
 )
