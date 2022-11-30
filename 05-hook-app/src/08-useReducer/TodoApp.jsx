b98750ef6d7ee22fa74e2c270e1fa80012c837c5
Saul Antonio Rodriguez Napoles <saul.rodriguez@mtk.com.mx>
2022-11-30 11:01:42 -0700

    Termino de sección 11

--- 05-hook-app/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -1,65 +1,14 @@
-import { useReducer, useEffect } from "react"
+import { useTodos } from "../hooks"
 import { TodoAdd } from "./components/TodoAdd"
 import { TodoList } from "./components/TodoList"
-import { todoReducer } from "./todoReducer"
-
-const initialState = [
-    {
-        id: new Date().getTime(),
-        description: 'Recolectar la piedra del alma',
-        done: false,
-    },
-    {
-        id: new Date().getTime() * 3,
-        description: 'Recolectar la piedra del tiempo',
-        done: false,
-    }
-];
-
-const init = () => {
-    return JSON.parse(localStorage.getItem('todos'))  || [];
-} 
-
 
 export const TodoApp = () => {    
 
-    const [ todos, dispatch ] = useReducer( todoReducer, initialState, init )
-
-    useEffect(() => {
-        localStorage.setItem('todos',JSON.stringify( todos  || [] ))
-    }, [todos])
-    
-
-    const handleNewTodo = ( todo ) => {
-        const action = {
-            type: '[TODO] Add Todo',
-            payload: todo
-        }
-
-        dispatch( action );
-    }
-
-    const handleDeleteTodo = ( id ) => {
-       
-        // console.log({ id })
-        dispatch({
-            type: '[TODO] Remove Todo',
-            payload: id
-        })
-    }
-
-    const handleToggleTodo = ( id ) => {
-    //    console.log({ id})
-        const action = {
-            type: '[TODO] Toggle Todo',
-            payload: id
-        }
-        dispatch( action );
-    }
-
+    const { todos, todosCount, pendingTodosCount ,handleNewTodo, handleDeleteTodo, handleToggleTodo } = useTodos();
+  
     return (
         <>
-            <h1>TodoApp 10, <small>pendientes: 2</small> </h1>
+            <h1>TodoApp {todos.length}, <small>pendientes: { todos.filter(todo => !todo.done).length }</small> </h1>
             <hr />
 
             <div className="row">
