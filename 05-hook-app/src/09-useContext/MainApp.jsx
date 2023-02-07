bef56f1f1751fb54daefa5ca859aa6fd507bbf1b
SaulNapoles <saul_antonio_16@hotmail.com>
2023-02-06 23:21:07 -0700

    Rutas y navbar

---
+++ 05-hook-app/src/09-useContext/MainApp.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom"
+import { Navbar } from "./Navbar"
+
+export const MainApp = () => {
+  return (
+    <>
+        <Navbar />
+        
+        <hr />
+
+        <Outlet />
+    </>
+  )
+}
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,24 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-// import { CallbackHook } from './06-memos/CallbackHook'
-// import { MemoHook } from './06-memos/MemoHook'
-// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
-// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
-// import { HooksApp } from './HooksApp'
-// import { CounterApp } from './01-useState/CounterApp'
-// import { SimpleForm } from './02-useEffect/SimpleForm'
-// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
-// import { FocusScreen } from './04-useRef/FocusScreen'
-// import { Layout } from './05-useLayoutEfect/Layout'
-// import { Memorize } from './06-memos/Memorize'
-// import { Padre } from './07-tarea-memo/07-tarea-memo/Padre'
-import { TodoApp } from './08-useReducer/TodoApp'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from "react-router-dom";
+import { getRoutes } from './Routes';
 
+import './index.css';
 
-import './index.css'
+const router  = getRoutes();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <TodoApp/>
-  // </React.StrictMode>
-)
+    <React.StrictMode>
+      <RouterProvider router={ router } />
+    </React.StrictMode>,
+);
