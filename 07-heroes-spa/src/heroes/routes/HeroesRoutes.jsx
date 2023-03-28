8921368af300b7e7850ad2e17355d5f687199777
SaulNapoles <saul_antonio_16@hotmail.com>
2023-03-27 20:43:54 -0700

    Rutas en React

---
+++ 07-heroes-spa/src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,25 @@
+import { DcPage, HeroPage, MarvelPage, SearchPage } from '../pages'
+import { Navbar } from '../../ui'
+import { Navigate, Route, Routes } from 'react-router-dom'
+
+export const HeroesRoutes = () => {
+  return (
+    <>
+        <Navbar/>
+
+        <div className="container">
+            <Routes>
+                <Route path="marvel" element={<MarvelPage />} />
+                <Route path="dc" element={<DcPage/>} />
+
+
+                <Route path="search" element={<SearchPage/>} />
+                <Route path="hero" element={<HeroPage/>} />
+
+                <Route path="/" element={<Navigate to="/login" />} />
+            </Routes>
+        </div>
+        
+    </>
+  )
+}
