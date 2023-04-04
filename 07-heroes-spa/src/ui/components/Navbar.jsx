2b3a936df203fc2731e39654f5fd72bd09c234af
SaulNapoles <saul_antonio_16@hotmail.com>
2023-04-03 23:16:50 -0700

    nombre usuario

--- 07-heroes-spa/src/ui/components/Navbar.jsx
+++ 07-heroes-spa/src/ui/components/Navbar.jsx
@@ -1,8 +1,13 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context/AuthContext';
+import { authReducer } from '../../auth/context/authReducer';
 
 
 export const Navbar = () => {
 
+    const { user } = useContext( AuthContext );
+    console.log(user);
     const navigate = useNavigate();
 
     const onLogout = () => {
@@ -10,6 +15,7 @@ export const Navbar = () => {
             replace: true
         });
     }
+
 
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
@@ -50,7 +56,7 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className="nav-item nav-link text-primary">
-                        Saul
+                        { user?.name }
                     </span>
 
                     <button
